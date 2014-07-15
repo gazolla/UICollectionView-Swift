f969124490e6f21f12af7f0a9f5b3b47a2479c79
Sebastiao Gazolla jr <gazolla@gmail.com>
2014-07-15 12:15:42 -0300

    updates

--- UICollectionView-Swift/AppDelegate.swift
+++ UICollectionView-Swift/AppDelegate.swift
@@ -16,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        // Override point for customization after application launch.
+        
+        let vc = ViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        
+        self.window!.rootViewController = nav
         self.window!.backgroundColor = UIColor.whiteColor()
         self.window!.makeKeyAndVisible()
         return true
---
+++ UICollectionView-Swift/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  UICollectionView-Swift
+//
+//  Created by Gazolla on 05/06/14.
+//  Copyright (c) 2014 Gazolla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    var collectionView:UICollectionView?
+    var flowLayout:UICollectionViewFlowLayout?
+    var items:NSMutableArray?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "CollectionView on Swift"
+        
+        self.items = NSMutableArray()
+        self.items?.addObjectsFromArray(["1"])
+        
+        let addBtn  = UIBarButtonItem (barButtonSystemItem: UIBarButtonSystemItem.Add,
+                                                    target: self,
+                                                    action: "addClicked")
+        
+        self.navigationItem.rightBarButtonItem = addBtn
+        
+        setupCollectionView()
+    }
+    
+    func addClicked() {
+        
+        self.collectionView?.performBatchUpdates({
+                let resultsSize = self.items?.count
+                self.items?.addObject("item")
+                let size = resultsSize! + 1
+                var arrayWithIndexPaths = NSMutableArray()
+                var i = 0
+                for (i = resultsSize!; i < resultsSize! + 1; i++) {
+                    arrayWithIndexPaths.addObject(NSIndexPath(forRow: i, inSection: 0))
+                }
+                self.collectionView?.insertItemsAtIndexPaths(arrayWithIndexPaths)
+            },
+            completion: nil)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupCollectionView(){
+        self.flowLayout = UICollectionViewFlowLayout()
+        self.flowLayout!.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        self.collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: self.flowLayout)
+        self.collectionView!.delegate = self
+        self.collectionView!.dataSource = self
+        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        self.collectionView!.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
+        self.view?.addSubview(self.collectionView)
+        
+    }
+
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize{
+        return CGSizeMake(310, 150)
+    }
+
+    
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int{
+        return self.items!.count
+    }
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        cell.layer.borderWidth = 0.5
+        cell.layer.borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1).CGColor
+        cell.layer.cornerRadius = 4
+        cell.backgroundColor = UIColor.whiteColor()
+        
+        return cell
+
+        
+    }
+
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
